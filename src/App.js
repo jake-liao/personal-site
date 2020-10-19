cae9a7731560b5e7da060b5c8bd87efa4e56c6e4
jake-liao <liaohsuehyu@gmail.com>
2020-10-18 20:22:02 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import "./style.css";
+// import { spacing } from "@material-ui/system";
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme)) => ({
+  grid: {
+    width: '100%',
+    margine: '0px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    background: them.palette.success.light,
+  }
+});
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Grid container spacing = {2} className = {classes.grid}>
+      <Grid item xs={6} md={12}>
+        <Paper className={classes.paper}>1</Paper>
+      </Grid>
+    </Grid>
   );
 }
