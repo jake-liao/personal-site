69417b7e37c3dc0e27215a93a88e7934d252ebb6
jake-liao <liaohsuehyu@gmail.com>
2020-10-20 00:04:17 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,19 +9,22 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
   AppBar: {
-    // padding: theme.spacing(2),
-    // textAlign: 'center',
+    width: "100vw",
     color: "white",
-    background: ""
+    background: "gray"
   },
   root: {
-    flexGrow: 1
+    flexGrow: "1",
   },
   menuButton: {
     marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1
+    flexGrow: "1"
+  },
+  img: {
+    width: "100vw",
+    height: "100vh"
   }
 }));
 
@@ -30,7 +33,7 @@ export default function App() {
   document.body.style.margin = "0px";
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar>
         <Toolbar>
           <IconButton
             edge="start"
@@ -39,6 +42,7 @@ export default function App() {
             aria-label="menu"
           >
             <MenuIcon />
+            
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             StarCluster
@@ -46,6 +50,7 @@ export default function App() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <img className={classes.img} position="static" src="https://i.picsum.photos/id/1021/2048/1206.jpg?hmac=fqT2NWHx783Pily1V_39ug_GFH1A4GlbmOMu8NWB3Ts" alt="new"/>
     </div>
   );
 }
